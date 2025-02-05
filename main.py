7f31cf0c09c0ae6edf651cb52467ec7a50b24365
Udayyy2003 <udayp1298@gmail.com>
2025-02-05 15:12:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+
+@app.route('/ecommerce')
+def ecommerce():
+    return render_template('ecommerce.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+app.run(debug=True)
